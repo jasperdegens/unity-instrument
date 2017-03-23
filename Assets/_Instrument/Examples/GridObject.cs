4e401118d8d60ea94acd334f255dd89e71fefd7a
TEAM-LAB\Jasper <jasper@team-lab.com>
2017-03-23 16:09:17 +0900

    added multitrack sequencer

---
+++ Assets/_Instrument/Examples/GridObject.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridObject : MonoBehaviour {
+
+    public int interval
+    {
+        get
+        {
+            return _interval;
+        }
+        set
+        {
+            _interval = Mathf.Clamp(value, intervalMin, intervalMax);
+        }
+    }
+    public int intervalMin = -4; // inclusive
+    public int intervalMax = 4;
+
+    private int _interval = 0;
+
+    public Color[] intervalColors;
+
+    public bool isActive = false;
+
+    private Color startColor;
+    private Color destColor;
+    private Material mat;
+
+    private Coroutine colorFade;
+    private void Start()
+    {
+
+        intervalColors = new Color[]
+        {
+            Color.red,
+            new Color(0.5f, 0, 1, 1),
+            Color.magenta,
+            new Color(1f, 0.5f, 0, 1),
+            new Color(1, .8f, 0, 1),
+            Color.green,
+            Color.cyan,
+            new Color(0, 0.5f, 1, 1),
+            Color.blue,
+        };
+
+        mat = gameObject.GetComponent<Renderer>().material;
+        startColor = mat.color;
+        destColor = new Color(1, .8f, 0, 1);
+        
+    }
+
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ChangeInterval(1);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            ChangeInterval(-1);
+        }
+    }
+
+    private void ChangeInterval(int changeAmount)
+    {
+        if (!isActive)
+        {
+            Activate();
+            return;
+        }
+        if (colorFade != null) StopCoroutine(colorFade);
+
+        if (interval + changeAmount > intervalMax || interval + changeAmount < intervalMin)
+        {
+            interval = 0;
+            isActive = false;
+        }
+        interval += changeAmount;
+        SetColor();
+    }
+
+    private void Activate()
+    {
+        isActive = true;
+        interval = 0;
+        SetColor();
+        
+    }
+    
+    private void SetColor()
+    {
+        if (isActive)
+        {
+            //int colorIndex = Mathf.Clamp(intervalColors.Length / 2 + interval, 0, intervalColors.Length - 1);
+            //mat.color = intervalColors[colorIndex];
+            //destColor = mat.color;
+        }
+        else
+        {
+            mat.color = startColor;
+        }
+    }
+
+    IEnumerator ActivateGrid(Color dest, Color orig)
+    {
+        float effectTime = 0.5f;
+        float currTime = effectTime;
+        while (currTime > 0)
+        {
+            currTime -= Time.deltaTime;
+            mat.color = Color.Lerp(orig, dest, currTime / effectTime);
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        mat.color = orig;
+        yield return null;
+    }
+
+    public void TriggerGrid()
+    {
+        if (isActive)
+        {
+            colorFade = StartCoroutine(ActivateGrid(Color.white, destColor));
+        } else
+        {
+            colorFade = StartCoroutine(ActivateGrid(new Color(.6f, .6f, .6f, 1), startColor));
+        }
+    }
+}
---
+++ Assets/_Instrument/Examples/SequencerMultiTrack.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using jasper.Music.Sequencer;
+
+public class SequencerMultiTrack : Sequencer {
+
+    public GameObject gridPrefab;
+    public int steps = 16;
+    public int tracks = 4;
+
+
+    private GridObject[,] grid;
+    private int _currPosition = 0;
+    private int currPosition
+    {
+        get
+        {
+            return _currPosition;
+        }
+        set
+        {
+            _currPosition = value % steps;
+        }
+    }
+
+
+    public override void Start()
+    {
+        base.Start();
+
+        grid = new GridObject[tracks,steps];
+
+        // center around transform midpoint
+        float startX = -(steps / 2.0f) + 0.5f;
+        float startY = (tracks / 2.0f) - 0.5f;
+        Vector3 startPos = new Vector3(startX, startY, 0);
+        for (int i = 0; i < tracks; i++)
+        {
+            for (int j = 0; j < steps; j++)
+            {
+                var go = Instantiate(gridPrefab, gameObject.transform);
+                go.transform.position = startPos + Vector3.down * i + Vector3.right * j;
+                grid[i, j] = go.GetComponent<GridObject>();
+            }
+        }
+
+    }
+
+    public override void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ToggleSequencer();
+        }
+        base.Update();
+    }
+
+    protected override void HandleBeat()
+    {
+        for (int i = 0; i < tracks; i++)
+        {
+            var g = grid[i, currPosition];
+            if (g.isActive)
+            {
+                g.TriggerGrid();
+                instrument.PlayNote(g.interval);
+            }
+            else
+            {
+                g.TriggerGrid();
+            }
+        }
+
+
+
+        currentBeat++;
+        totalBeats++;
+        if (currentBeat > timeSignature)
+        {
+            currentBeat = 0;
+        }
+        currPosition++;
+        currNotePosition++;
+
+        if (debug)
+        {
+            print(currentBeat);
+        }
+
+    }
+}
